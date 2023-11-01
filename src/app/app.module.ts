8fed8959035ae6dd952899e39844d5e2798e5dff
Matt Payne <Payne@MattPayne.org>
2023-10-31 19:44:34 -0500

    ng generate component answer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionsComponent } from './questions/questions.component';
+import { AnswerComponent } from './answer/answer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionsComponent
+    QuestionsComponent,
+    AnswerComponent
   ],
   imports: [
     BrowserModule,
